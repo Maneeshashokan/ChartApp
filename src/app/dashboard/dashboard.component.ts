75cea8ed766025f131d9ae2c4a51dd7653a0d90b
Bijoy Baby <bijoybabys@gmail.com>
2019-08-10 03:04:12 +0530

    Bijoy: updates

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -62,129 +62,19 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
     this.user = JSON.parse(window.localStorage.getItem("user"));
+    let token = JSON.parse(window.localStorage.getItem("access_token"));
     this.showLoader = true;
-    this.sharedService.httpGet("getCharts").subscribe(res => {
+    this.sharedService.httpGet("/api/datachart/" +token).subscribe(res => {
       this.showLoader = false;
-      if (res.content) {
-        // this.data = {
-        //   chart1:{
-        //     labels:["MAT Sprint 1","MAT Sprint 2","MAT Sprint 3","MAT Sprint 4"],
-        //     datasets:[
-        //           {
-        //               label: 'Velocity-Velocity Predictability',
-        //               backgroundColor: '#ff8302',
-        //               borderColor: '#ff8302',
-        //               width:10,
-        //               data: [7, 8, 23,20]
-        //           },
-        //           {
-        //             label: '',
-        //             backgroundColor: '#ccc0',
-        //             borderColor: '#ccc0',
-        //             data: [0,0, 0, 0]
-        //         }
-               
-              
-        //     ]
-        //   },
-        //   chart2:{
-        //     labels:["MAT Sprint 1","MAT Sprint 2","MAT Sprint 3","MAT Sprint 4"],
-        //     datasets:[
-        //           {
-        //               label: 'Accepted/Commited',
-        //               backgroundColor: '#ff8302',
-        //               borderColor: '#ff8302',
-        //               data: [100, 133, 100,100]
-        //           },
-        //           {
-        //             label: '',
-        //             backgroundColor: '#ccc0',
-        //             borderColor: '#ccc0',
-        //             data: [0,0, 0, 0]
-        //         }
-               
-              
-        //     ]
-        //   },
-        //   chart3:{
-        //     labels:["MAT Sprint 1","MAT Sprint 2","MAT Sprint 3","MAT Sprint 4"],
-        //     datasets:[
-        //           {
-        //               label: 'Added Scope',
-        //               backgroundColor: '#f3098b',
-        //               borderColor: '#f3098b',
-        //               data: [0, 17, 0, 0]
-        //           },
-        //           {
-        //             label: 'Commited Work',
-        //             backgroundColor: '#ff8302',
-        //             borderColor: '#ff8302',
-        //             data: [12, 14, 23, 20]
-        //         },
-        //         {
-        //           label: 'Removed Scope',
-        //           backgroundColor: '#2487e1',
-        //           borderColor: '#ff8302',
-        //           data: [0, 0, 0, 0]
-        //       },
-        //       {
-        //         label: '',
-        //         backgroundColor: '#ccc0',
-        //         borderColor: '#ccc0',
-        //         data: [0,0, 0, 0]
-        //     }
-               
-              
-        //     ]
-        //   },
-        //   chart4:{
-        //     labels:["MAT Sprint 1","MAT Sprint 2","MAT Sprint 3","MAT Sprint 4"],
-        //     datasets:[
-        //           {
-        //               label: 'Lowest',
-        //               backgroundColor: '#5a0621',
-        //               borderColor: '#5a0621',
-        //               data: [1, 0, 0, 0]
-        //           },
-        //           {
-        //             label: 'Heigh',
-        //             backgroundColor: '#ff8302',
-        //             borderColor: '#ff8302',
-        //             data: [2, 1, 2, 5]
-        //         },
-        //         {
-        //           label: 'Low',
-        //           backgroundColor: '#06c',
-        //           borderColor: '#06c',
-        //           data: [3, 0, 3, 0]
-        //       },
-        //       {
-        //         label: 'Heighest',
-        //         backgroundColor: '#8dcdff',
-        //         borderColor: '#8dcdff',
-        //         data: [5, 2, 4, 0]
-        //     },
-        //     {
-        //       label: 'Medium',
-        //       backgroundColor: '#f32a09',
-        //       borderColor: '#f32a09',
-        //       data: [0, 3, 5, 5]
-        //   }
-               
-              
-        //     ]
-        //   }
-          
-        // };
-        this.data = res.content
+      
+         
+        this.data = res.data
         this.showChart = true;
         setTimeout(() => {
           this.barChart.reinit();
-          this.pieChart.reinit();
-          this.lineChart.reinit();
-          this.doughnutChart.reinit();
+          
         });
-      }
+      
     });
   }
 }
